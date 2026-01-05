98298a4626c5f4f47fe19fa30137e6d933211fc5
choieunsu2009 <jogg86199@gmail.com>
2026-01-05 14:08:28 +0900

    5교시 중간 체크포인트

---
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -0,0 +1,73 @@
+package com.example.firstproject.api;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.ArticleService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+public class ArticleApiController {
+    @Autowired
+    private ArticleService articleService;
+
+    @GetMapping("/api/articles")
+    public List<Article> index(){
+        return articleService.index;
+    }
+
+
+
+//    @Autowired
+//    private ArticleRepository articleRepository;
+//
+//    // Get
+//    @GetMapping("/api/articles")
+//    public List<Article> index() {
+//        return articleRepository.findAll();
+//    }
+//
+//    @GetMapping("/api/articles/{id}")
+//    public Article show(@PathVariable Long id){
+//        return articleRepository.findById(id).orElse(null);
+//    }
+//    // Post
+//    @PostMapping("/api/articles")
+//    public Article create(@RequestBody ArticleForm dto){
+//        Article article = dto.toEntity();
+//        return articleRepository.save(article);
+//    }
+//    // Patch
+//    @PatchMapping("/api/articles/{id}")
+//    public ResponseEntity<Object> update(@PathVariable Long id, @RequestBody ArticleForm dto){
+//        Article article = dto.toEntity();
+//        log.info("id : {}, article : {}", id, article.toString());
+//
+//        Article target = articleRepository.findById(id).orElse(null);
+//        if(target == null || !id.equals(article.getId())){
+//            log.info("잘못된 요청 id : {}, article : {}", id, article.toString());
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//
+//        Article updated = articleRepository.save(article);
+//        return ResponseEntity.status(HttpStatus.OK).body(updated);
+//    }
+//    // Delete
+//    @DeleteMapping("/api/articles/{id}")
+//    public ResponseEntity<Article> delete(@PathVariable Long id){
+//        Article target = articleRepository.findById(id).orElse(null);
+//
+//        if(target == null){
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//        articleRepository.delete(target);
+//        return ResponseEntity.status(HttpStatus.OK).body(null);
+//    }
+}
